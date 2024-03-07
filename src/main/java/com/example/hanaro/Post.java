22cf2fd81f7f20ba2ba9928dc6f13dc8d1d65670
chansoo <chansoo38@gmail.com>
2024-03-07 16:49:45 +0900

    nothing

---
+++ src/main/java/com/example/hanaro/Post.java
@@ -0,0 +1,2 @@
+package com.example.hanaro;public class Post {
+}
---
+++ src/main/java/com/example/hanaro/request/PostDto.java
@@ -0,0 +1,10 @@
+package com.example.hanaro.request;
+
+import lombok.Getter;
+
+@Getter
+public class PostDto {
+    String title;
+    String writer;
+    String contents;
+}
